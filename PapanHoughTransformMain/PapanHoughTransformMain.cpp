7696166e2223c29fb7e7f9a1bba198c9f9c2eea0
konstantysz <kostekszumigaj@gmail.com>
2021-11-06 15:57:06 +0100

    Create ImageAnalysis lib

--- PapanHoughTransformMain/PapanHoughTransformMain.cpp
+++ PapanHoughTransformMain/PapanHoughTransformMain.cpp
@@ -3,8 +3,10 @@
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 
+#include "ImageAnalyzer.h"
+
 int main()
 {
     auto xd = cv::imread(".//..//Resources//testImage.jpg");
-    std::cout << "Hello World!\n";
+
 }

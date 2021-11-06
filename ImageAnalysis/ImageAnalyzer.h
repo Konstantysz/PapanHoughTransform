06199cc162f0f5be02864267411b9f85cb2016a0
konstantysz <kostekszumigaj@gmail.com>
2021-11-06 15:58:27 +0100

    Create abstract ImageAnalyzer class

--- ImageAnalysis/ImageAnalyzer.h
+++ ImageAnalysis/ImageAnalyzer.h
@@ -1,1 +1,11 @@
 #pragma once
+
+namespace ImageAnalysis {
+	class ImageAnalyzer
+	{
+		virtual cv::Mat GaussianBlur(const cv::Mat& input) = 0;
+		virtual cv::Mat Image2Grayscale(const cv::Mat& input) = 0;
+		virtual cv::Mat Canny(const cv::Mat& input) = 0;
+		virtual cv::Mat CircleHoughTransform(const cv::Mat& input) = 0;
+	};
+}

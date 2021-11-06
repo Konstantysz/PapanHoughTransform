2b46c6503390c5f82a1600913b25e63a18b8ecf4
konstantysz <kostekszumigaj@gmail.com>
2021-11-06 15:35:28 +0100

    Created project

---
+++ PapanHoughTransformMain/PapanHoughTransformMain.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+
+int main()
+{
+    auto xd = cv::imread(".//..//Resources//testImage.jpg");
+    std::cout << "Hello World!\n";
+}

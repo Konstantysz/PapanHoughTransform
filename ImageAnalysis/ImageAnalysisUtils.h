5a6c289eccf037f5a214fe68bf5fa6dedefd78d6
konstantysz <kostekszumigaj@gmail.com>
2021-11-08 00:42:24 +0100

    Add helpers to utils

--- ImageAnalysis/ImageAnalysisUtils.h
+++ ImageAnalysis/ImageAnalysisUtils.h
@@ -5,7 +5,17 @@ namespace ImageAnalysis
 	namespace utils
 	{
 		void SingleConvolve(const cv::Mat& input, cv::Mat& output, const cv::Mat& kernel, const int& i, const int& j);
+		
+		cv::Mat Convolve(const cv::Mat& input, const cv::Mat& kernel);
 
 		cv::Mat FilterKernelGenerator(int size);
+
+		std::pair<cv::Mat, cv::Mat> Gradient(cv::Mat input);
+
+		cv::Mat NonMaxSuppression(const cv::Mat& gradientIntensity, const cv::Mat& gradientDirection);
+
+		cv::Mat DoubleThreshold(const cv::Mat& input, float lowThresholdRatio = 0.05, float highThresholdRatio = 0.09);
+
+		cv::Mat HysteresisThresholding(const cv::Mat& input);
 	} // namespace utils
 } // namespace ImageAnalysis

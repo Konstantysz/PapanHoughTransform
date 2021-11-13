252516c6b7e2eb232e48974253e4e41c451089a5
konstantysz <kostekszumigaj@gmail.com>
2021-11-13 16:15:33 +0100

    Correct tests

--- PapanHoughTransformTests/test.cpp
+++ PapanHoughTransformTests/test.cpp
@@ -24,11 +24,11 @@ protected:
 	std::shared_ptr<cv::Mat> testImage;
 	std::shared_ptr<ImageAnalysis::ImageAnalyzerSingleThread> imageAnalyzer;
 	
-	double GetSimilarity(const cv::Mat& A, const cv::Mat& B) {
+	double GetSimilarityRMS(const cv::Mat& A, const cv::Mat& B) {
 		if (A.rows > 0 && A.rows == B.rows && A.cols > 0 && A.cols == B.cols) {
 			double errorL2 = norm(A, B, cv::NORM_L2);
 
-			return errorL2 / double(A.rows * A.cols);
+			return std::sqrt(errorL2 / double(A.rows * A.cols));
 		}
 		else {
 			//Images have a different size
@@ -38,25 +38,32 @@ protected:
 
 };
 
-TEST_F(TestImageAnalyzerSingleThread, BGR2Gray) {
+TEST_F(TestImageAnalyzerSingleThread, BGR2Gray) 
+{
 	cv::Mat cvResult; 
 	cv::cvtColor(*testImage, cvResult, cv::COLOR_BGR2GRAY);
 
 	cv::Mat iaResult = imageAnalyzer->BGR2Grayscale(*testImage);
-	double similarity = GetSimilarity(cvResult, iaResult);
+	double error = GetSimilarityRMS(cvResult, iaResult);
 
-	EXPECT_TRUE(similarity < 0.01);
+	EXPECT_TRUE(error < 0.1);
 }
 
-TEST_F(TestImageAnalyzerSingleThread, GaussianBlur) {
-	int n = 3;
-	cv::Mat cvResult;
-	cv::GaussianBlur(*testImage, cvResult, cv::Size(n, n), 1);
+TEST_F(TestImageAnalyzerSingleThread, GaussianBlur) 
+{	
+	std::vector<double> errors;
+	for (int n = 1; n < 15; n += 2)
+	{
+		cv::Mat cvResult;
+		cv::GaussianBlur(*testImage, cvResult, cv::Size(n, n), 1);
 
-	cv::Mat iaResult = imageAnalyzer->GaussianBlur(*testImage, n, 1);
-	double similarity = GetSimilarity(cvResult, iaResult);
+		cv::Mat iaResult = imageAnalyzer->GaussianBlur(*testImage, n, 1);
+		errors.push_back(GetSimilarityRMS(cvResult, iaResult));
+	}
 
-	EXPECT_TRUE(similarity < 0.01);
+	auto maxError = *std::max_element(errors.begin(), errors.end());
+
+	EXPECT_TRUE(maxError < 0.1);
 }
 
 int main(int argc, char** argv) {

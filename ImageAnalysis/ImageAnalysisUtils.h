b9f077207d4d54bb8a9e10af36515827a7a7625b
konstantysz <kostekszumigaj@gmail.com>
2021-11-18 22:36:20 +0100

    Add Timer class

--- ImageAnalysis/ImageAnalysisUtils.h
+++ ImageAnalysis/ImageAnalysisUtils.h
@@ -1,9 +1,28 @@
 #pragma once
+#include <chrono>
 
 namespace ImageAnalysis
 {
 	namespace utils
 	{
+		class Timer
+		{
+		public:
+			Timer(std::string name)
+			{
+				functionName = name;
+				tic = std::chrono::steady_clock::now();
+			}
+			~Timer()
+			{
+				auto toc = std::chrono::steady_clock::now();
+				std::cout << "[" + functionName + "] Time elapsed: " << std::chrono::duration_cast<std::chrono::microseconds>(toc - tic).count() << "[µs]" << std::endl;
+			}
+		private:
+			std::string functionName;
+			std::chrono::steady_clock::time_point tic;
+		};
+
 		void SingleConvolve(const cv::Mat& input, cv::Mat& output, const cv::Mat& kernel, const int& i, const int& j);
 		
 		cv::Mat ConvolveZeroPad(const cv::Mat& input, const cv::Mat& kernel);
@@ -19,5 +38,6 @@ namespace ImageAnalysis
 		cv::Mat DoubleThreshold(const cv::Mat& input, float lowThresholdRatio = 0.05, float highThresholdRatio = 0.09);
 
 		cv::Mat HysteresisThresholding(const cv::Mat& input);
+
 	} // namespace utils
 } // namespace ImageAnalysis
